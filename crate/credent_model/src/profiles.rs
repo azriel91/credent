36256870c6c02403ed0eb526a5778e94e0e2d683
Azriel Hoh <azriel91@gmail.com>
2020-10-10 17:25:59 +1300

    Implement `From` between `Profiles` and `BTreeMap`.

--- crate/credent_model/src/profiles.rs
+++ crate/credent_model/src/profiles.rs
@@ -1,13 +1,20 @@
 use std::{
-    collections::BTreeSet,
+    collections::{BTreeMap, BTreeSet},
     fmt::{self, Display},
     ops::{Deref, DerefMut},
 };
 
-use crate::Profile;
+use crate::{Credentials, Profile};
 
 /// Set of `Profile`s. `BTreeSet` newtype.
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "serde",
+    serde(
+        from = "BTreeMap<String, Credentials>",
+        into = "BTreeMap<String, Credentials>"
+    )
+)]
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Profiles(pub BTreeSet<Profile>);
 
@@ -36,5 +43,35 @@ impl Display for Profiles {
         profile_iter.try_for_each(|profile| write!(f, ", {}", profile))?;
 
         write!(f, "]")
+    }
+}
+
+impl From<BTreeMap<String, Credentials>> for Profiles {
+    fn from(profile_map: BTreeMap<String, Credentials>) -> Self {
+        let profiles_set = profile_map
+            .into_iter()
+            .map(|(name, credentials)| Profile::new(name, credentials))
+            .collect();
+
+        Self(profiles_set)
+    }
+}
+
+impl From<Profiles> for BTreeMap<String, Credentials> {
+    fn from(profiles: Profiles) -> Self {
+        profiles
+            .0
+            .into_iter()
+            .map(|profile| (profile.name, profile.credentials))
+            .collect()
+    }
+}
+
+impl<'p> From<&'p Profiles> for BTreeMap<&'p str, &'p Credentials> {
+    fn from(profiles: &'p Profiles) -> Self {
+        profiles
+            .iter()
+            .map(|profile| (profile.name.as_str(), &profile.credentials))
+            .collect::<BTreeMap<&str, &Credentials>>()
     }
 }
