f26efefc5330676f76e485885aebea22fca134c9
Azriel Hoh <azriel91@gmail.com>
2021-03-20 21:10:55 +1300

    Add tests to `CredentialsFileLoader`.

--- crate/credent_fs/src/credentials_file_loader.rs
+++ crate/credent_fs/src/credentials_file_loader.rs
@@ -127,3 +127,124 @@ where
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+
+    use credent_fs_model::Error;
+    use credent_model::{Credentials, Password, Profile, Profiles, Username};
+    use tempfile::NamedTempFile;
+
+    use super::CredentialsFileLoader;
+
+    #[cfg(feature = "base64")]
+    const PROFILES_CONTENT: &str = r#"
+        [default]
+        username = "me"
+        password = "c2VjcmV0" # secret
+
+        [profile_other]
+        username = "you"
+        password = "Y29kZQ==" # code
+    "#;
+
+    #[cfg(not(feature = "base64"))]
+    const PROFILES_CONTENT: &str = r#"
+        [default]
+        username = "me"
+        password = "secret"
+
+        [profile_other]
+        username = "you"
+        password = "code"
+    "#;
+
+    #[test]
+    fn loads_credentials() -> Result<(), Box<dyn std::error::Error>> {
+        let mut file = NamedTempFile::new()?;
+        write!(file, "{}", PROFILES_CONTENT)?;
+
+        let profiles: Profiles = smol::block_on(CredentialsFileLoader::load_file(file.path()))?;
+        let profile_default = profiles.get(Profile::<Credentials>::DEFAULT_NAME);
+        let profile_other = profiles.get("profile_other");
+
+        let profile_default_expected = Profile::new_default(Credentials {
+            username: Username(String::from("me")),
+            password: Password::new("secret"),
+        });
+        assert_eq!(Some(&profile_default_expected), profile_default);
+
+        let profile_other_expected = Profile::new(
+            String::from("profile_other"),
+            Credentials {
+                username: Username(String::from("you")),
+                password: Password::new("code"),
+            },
+        );
+        assert_eq!(Some(&profile_other_expected), profile_other);
+
+        Ok(())
+    }
+
+    #[test]
+    fn returns_err_file_non_existent_when_file_not_exist() -> Result<(), Box<dyn std::error::Error>>
+    {
+        let file = NamedTempFile::new()?;
+        let path = file.path().to_path_buf();
+        file.close()?;
+
+        let load_result = smol::block_on(CredentialsFileLoader::<Credentials>::load_file(&path));
+
+        if let Err(Error::CredentialsFileNonExistent { credentials_path }) = &load_result {
+            assert_eq!(&path, credentials_path);
+        } else {
+            panic!("Expected `load_result` to return `CredentialsFileNonExistent` error, but got `{:?}`.", load_result);
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn returns_err_file_is_dir_when_file_is_directory() -> Result<(), Box<dyn std::error::Error>> {
+        let temp_dir = tempfile::tempdir()?;
+        let path = temp_dir.path();
+
+        let load_result = smol::block_on(CredentialsFileLoader::<Credentials>::load_file(path));
+
+        if let Err(Error::CredentialsFileIsDir { credentials_path }) = &load_result {
+            assert_eq!(path, credentials_path);
+        } else {
+            panic!(
+                "Expected `load_result` to return `CredentialsFileIsDir` error, but got `{:?}`.",
+                load_result
+            );
+        }
+
+        Ok(())
+    }
+    #[test]
+    fn returns_err_deserialize_when_contents_are_broken() -> Result<(), Box<dyn std::error::Error>>
+    {
+        let mut file = NamedTempFile::new()?;
+        write!(file, "garbage")?;
+        let path = file.path();
+
+        let load_result = smol::block_on(CredentialsFileLoader::<Credentials>::load_file(path));
+
+        if let Err(Error::CredentialsFileDeserialize {
+            credentials_path,
+            error: _,
+        }) = &load_result
+        {
+            assert_eq!(path, credentials_path);
+        } else {
+            panic!(
+                "Expected `load_result` to return `CredentialsFileDeserialize` error, but got `{:?}`.",
+                load_result
+            );
+        }
+
+        Ok(())
+    }
+}
